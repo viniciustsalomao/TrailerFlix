6cc970cc464daab0b256a46769b5136aafea9fe9
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-22 19:13:15 -0300

    Initial Commit

---
+++ TrailerFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailerFlix
+//
+//  Created by Vinícius Tinajero Salomão on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
